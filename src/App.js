69a4b0aa4ded1ce41a539816da04bbf898076724
Annshuk <annshuks@gmail.com>
2021-11-17 22:08:12 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useForm, FormProvider } from 'react-hook-form';
 import { useStateMachine } from 'little-state-machine';
 
@@ -18,6 +18,13 @@ import './style.css';
  *
  * how you can prevent re-render
  */
+
+const intialValue = {
+  test1: true,
+  test2: false,
+  test3: false,
+};
+
 const App = () => {
   const {
     register,
@@ -25,6 +32,8 @@ const App = () => {
     formState: { errors = {} },
     ...restProps
   } = useForm({ mode: 'onBlur', reValidateMode: 'onBlur' });
+
+  const [formData, setForm] = useState(intialValue);
 
   const { actions, state } = useStateMachine({ updateNameAction });
 
@@ -36,55 +45,84 @@ const App = () => {
     actions.updateNameAction({ name: event.target.value });
   }, []);
 
-  const col = React.useMemo(
-    () => [
-      {
-        Header: 'Name',
-        columns: [
-          {
-            Header: 'First Name',
-            accessor: 'firstName',
-          },
-          {
-            Header: 'Last Name',
-            accessor: 'lastName',
-          },
-        ],
-      },
-      {
-        Header: 'Info',
-        columns: [
-          {
-            Header: 'Age',
-            accessor: 'age',
-          },
-          {
-            Header: 'Visits',
-            accessor: 'visits',
-          },
-          {
-            Header: 'Status',
-            accessor: 'status',
-          },
-          {
-            Header: 'Profile Progress',
-            accessor: 'progress',
-          },
-        ],
-      },
-    ],
-    []
-  );
-  const data = React.useMemo(() => makeData(200), []);
+  // const col = React.useMemo(
+  //   () => [
+  //     {
+  //       Header: 'Name',
+  //       columns: [
+  //         {
+  //           Header: 'First Name',
+  //           accessor: 'firstName',
+  //         },
+  //         {
+  //           Header: 'Last Name',
+  //           accessor: 'lastName',
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       Header: 'Info',
+  //       columns: [
+  //         {
+  //           Header: 'Age',
+  //           accessor: 'age',
+  //         },
+  //         {
+  //           Header: 'Visits',
+  //           accessor: 'visits',
+  //         },
+  //         {
+  //           Header: 'Status',
+  //           accessor: 'status',
+  //         },
+  //         {
+  //           Header: 'Profile Progress',
+  //           accessor: 'progress',
+  //         },
+  //       ],
+  //     },
+  //   ],
+  //   []
+  // );
+  // const data = React.useMemo(() => makeData(200), []);
+  const allSelected = Object.values(formData).every(Boolean);
+
+  console.log(formData);
+
+  const handlChecked = ({ target }) => {
+    setForm({ ...formData, [target.name]: target.checked });
+  };
+
+  const handleAllChecked = () => {
+    setForm((prevState) =>
+      Object.keys(prevState).reduce(
+        (prev, key) => ({ ...prev, ...prev[key], [key]: !allSelected }),
+        {}
+      )
+    );
+  };
 
   return (
     <FormProvider {...{ register, handleSubmit, errors, ...restProps }}>
       <Greet name={state.name} />
       <br />
       <GreetName onChange={handleChange} />
-      <Styles>
-        <Table columns={col} data={data} />
-      </Styles>
+      <button onClick={handleAllChecked}>
+        {!allSelected ? 'Select All' : 'unSelect All'}
+      </button>
+
+      <br />
+      {Object.entries(formData).map(([key, value]) => {
+        return (
+          <input
+            type="checkbox"
+            checked={value}
+            key={key}
+            name={key}
+            onChange={handlChecked}
+          />
+        );
+      })}
     </FormProvider>
   );
 };
