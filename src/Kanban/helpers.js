284de407ef2f9b919b3cc9bd5031c4ca2a54917f
Annshuk <annshuks@gmail.com>
2022-01-30 17:02:58 +0530

    1 file modified

--- src/Kanban/helpers.js
+++ src/Kanban/helpers.js
@@ -1,8 +1,16 @@
-const goingForward = (newTask, id, moveTask, removeItem) => {
+/**
+ * goingForward
+ * @param {Array} //item
+ * @param {Number} //id,
+ * @param {Array} //moveTask,
+ * @param {Array} //removeItem
+ * @return newTask Board for forward
+ */
+const goingForward = (newTask, id, moveTask, removeItem, stage) => {
   newTask[id].cards = [...removeItem];
 
   newTask[id + 1].cards = [
-    ...newTask[direction].cards,
+    ...newTask[id + 1].cards,
     {
       ...moveTask,
       stage: stage < 3 ? stage + 1 : 3,
@@ -12,7 +20,16 @@ const goingForward = (newTask, id, moveTask, removeItem) => {
   return newTask;
 };
 
-const goingBackward = (newTask, id, moveTask, removeItem) => {
+/**
+ *
+ * goingBackward
+ * @param {Array} //item
+ * @param {Number} //id,
+ * @param {Array} //moveTask,
+ * @param {Array} //removeItem
+ * @return newTask Board for back
+ */
+const goingBackward = (newTask, id, moveTask, removeItem, stage) => {
   newTask[id].cards = [...removeItem];
 
   newTask[id - 1].cards = [
@@ -43,8 +60,8 @@ const moveOrBack = (tasks, id, itemId, action) => {
 
   // is next of previous task
   const direction = isForward
-    ? goingForward(newTask, id, moveTask, removeItem)
-    : goingBackward(newTask, id, moveTask, removeItem);
+    ? goingForward(newTask, id, moveTask, removeItem, stage)
+    : goingBackward(newTask, id, moveTask, removeItem, stage);
 
   return newTask;
 };
