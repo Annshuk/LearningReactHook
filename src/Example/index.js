d1845f2b6cc4056b34653f7d60f5cb86daa3fb25
Annshuk <annshuks@gmail.com>
2022-06-14 10:45:39 +0530

    1 file modified

--- src/Example/index.js
+++ src/Example/index.js
@@ -11,7 +11,6 @@ import { Textarea } from '../Textarea';
 
 import Greet from './Greet';
 import GreetName from './GreetName';
-import { StackText } from '../StackText';
 
 const totalCount = 90;
 /**
@@ -50,7 +49,6 @@ const Example = () => {
       <GreetName onChange={handleChange} />
       <CheckUnchecked />
       <Textarea id="textFields" maxLength={totalCount} />
-      <StackText />
     </FormProvider>
   );
 };
